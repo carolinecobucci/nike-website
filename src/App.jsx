27ae861dbde7983ad1661aa2da80a42c2dbbf610
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-12-01 17:52:46 -0300

    created components and sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
-function App() {
-  return <div>App</div>;
-}
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections/index";
+import Nav from "./components/Nav";
+
+const App = () => {
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x p-y-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8 ">
+        <Footer />
+      </section>
+    </main>
+  );
+};
 
 export default App;
